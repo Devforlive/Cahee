8e7d3fecb16e519fefbba74841e10a92fd3d036f
«Devforlive» <e_mail_post_box@yahoo.com>
2017-08-27 12:55:29 +0300

    Fixed

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$('.slide__h-menu').on('click', 'a', function(even){
+	even.preventDefault();
+	var link = $(this).attr('href');
+	var distanse = $(link).offset().top;
+	$('html, body').animate({
+		scrollTop: distanse
+	}, 600);
+});
+
+$('.slide__next-link').click(function(){
+	var link = $(this).attr('href');
+	var distanse = $(link).offset().top;
+	$('html, body').animate({
+		scrollTop: distanse
+	}, 500);
+});
+
+$('.footer__logo').on('click', function(){
+	var link = $(this).attr('href');
+	var distanse = $(link).offset().top;
+	$('html, body').animate({
+		scrollTop: distanse
+	}, 700);
+});
+
+
+$(window).scroll(function(){
+	var distanse_menu = $('#about__us').offset().top;
+	if ($(window).scrollTop() > distanse_menu){
+		$('header').css('background','rgba(250,250,250, 0.7)');
+	}
+	else {
+		$('header').css('background','transparent');
+	}
+});
+
+$('#show-menu').on('click', function(){
+	if($('.slide__h-menu').hasClass('open')){
+		$('.slide__h-menu').removeClass('open');
+		$('#show-menu').find('span')
+		.removeClass('icon-cross')
+		.addClass('icon-menu');
+	}else{
+		$('.slide__h-menu').addClass('open');
+		$('#show-menu').find('span')
+		.removeClass('icon-menu')
+		.addClass('icon-cross');
+	}      
+});
+
+$(window).resize(function(){
+	if ($(window).width() > 768){
+		if($('.slide__h-menu').hasClass('open')){
+			$('.slide__h-menu').removeClass('open')
+			$('#show-menu').find('span')
+			.removeClass('icon-cross')
+			.addClass('icon-menu');
+		}
+	}
+});
